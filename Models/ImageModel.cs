7770ec64014bcc1daa995f7e6caa9377d2938629
Emmanuel CHRIQUI <git@chriqui.name>
2024-09-18 19:29:02 +0200

    Add Image model

---
+++ Models/ImageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Models
+{
+    public class ImageModel
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public byte[] OriginalImage { get; set; }
+        public byte[] ProcessedImage { get; set; }
+        public DateTime UploadedAt { get; set; }
+    }
+}
