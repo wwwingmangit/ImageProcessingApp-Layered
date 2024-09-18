606dd6d2d3c3b963c14518d3567e986984809c4e
Emmanuel CHRIQUI <git@chriqui.name>
2024-09-18 19:47:09 +0200

    Add Presentation Layer

---
+++ PresentationLayer/Controllers/ImageController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using BusinessLogicLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace PresentationLayer.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly ImageService _imageService;
+
+        public ImageController()
+        {
+            _imageService = new ImageService();
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    await file.CopyToAsync(ms);
+                    var imageData = ms.ToArray();
+                    var imageModel = await _imageService.UploadImageAsync(imageData, file.FileName);
+                    return RedirectToAction("Details", new { id = imageModel.Id });
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var imageModel = await _imageService.ApplyFilterAsync(id, "grayscale");
+            return View(imageModel);
+        }
+    }
+}
--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
@@ -9,7 +10,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Image}/{action=Upload}/{id?}");
 
 app.Run();
